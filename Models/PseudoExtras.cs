c81d85020d9636f80436445195bcc4308ab9b44a
sasha-poirier-pix4d <sasha.poirier@pix4d.com>
2024-03-06 13:22:54 +0100

    Better random satellites & clean

--- Models/PseudoExtras.cs
+++ Models/PseudoExtras.cs
@@ -22,8 +22,16 @@ namespace PseudoExtras {
     ) {
         public static Array Values = Enum.GetValues(typeof(SatelliteType));
         private static readonly Random RNG = new ();
-        public static Satellite Random() {
+        public static Satellite RandomSatellite() {
             return new Satellite(Id: RNG.Next(),Elv: RNG.Next(0, 180), Azm: RNG.Next(0, 360), Snr: RNG.Next(0, 120), true, (SatelliteType)Values.GetValue(RNG.Next(Values.Length)));
+        }
+        public static Satellite[] RandomSatellites() {
+            var satellites = new Satellite[RNG.Next(0, 10)];
+            for (int i = 0; i < satellites.Length; i++)
+            {
+                satellites[i] = Satellite.RandomSatellite();
+            }
+            return satellites;
         }
     }
 
@@ -37,10 +45,7 @@ namespace PseudoExtras {
         string geoidModel, double mslHeight, double undulation,
         float utcTime, string gpsTimeStamp, string utcTimeStamp,
         PseudoExtras.SubscriptionType subscriptionType,
-        int satellites, int totalSatInView, 
-        //Incorrect structure from documentation
-        //int satellitesView,
-        //int[] satellitesId, int[] satellitesElv, int[] satellitesAzm, int[] satellitesSnr, bool[] satellitesUse, SatelliteType[] satellitesType/*, Satellite[] satellitesMerged*/)
+        int satellites, int totalSatInView,
         PseudoExtras.Satellite[] satelliteView)
     {
         static PseudoExtrasMessage MakeZero() {
@@ -52,7 +57,6 @@ namespace PseudoExtras {
                 subscriptionType: SubscriptionType.Free,
                 satellites: 0, totalSatInView: 0, 
                 satelliteView: []);
-                //satellitesView: 0, satellitesId: new int[0], satellitesElv: new int[0], satellitesAzm: new int[0], satellitesSnr: new int[0], satellitesUse: new bool[0], satellitesType: new SatelliteType[0]);
         }
         
         public static PseudoExtrasMessage MakeNearMalley() {
@@ -62,7 +66,6 @@ namespace PseudoExtras {
             var lon = 6.600880505618732 - (random.NextDouble() * (0.001 - -0.001) + -0.001);
             var millis = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
 
-            //After 10 messages or so, send corrected...
             return new PseudoExtrasMessage(latitude: lat, longitude: lon, altitude: 400, speed: 0, 
                 bearing: 0, accuracy: 0, verticalAccuracyMeters: 0, hdop: 0, vdop: 0, pdop: 0,
                 diffAge: 0, diffStatus: DiffStatus.Unknown, diffID: 0,
@@ -70,8 +73,7 @@ namespace PseudoExtras {
                 utcTime: millis, gpsTimeStamp: "2024-02-13T14:49:49.0000000", utcTimeStamp: "2024-02-13T14:49:49.0000000",
                 subscriptionType: SubscriptionType.Free,
                 satellites: 0, totalSatInView: 0,
-                satelliteView: [new Satellite(1, 20, 20, 20, false, SatelliteType.GPS)]);
-                //satellitesView: 0, satellitesId: new int[0], satellitesElv: new int[0], satellitesAzm: new int[0], satellitesSnr: new int[0], satellitesUse: new bool[0], satellitesType: new SatelliteType[0]);
+                satelliteView: Satellite.RandomSatellites());
         }
     }
 }
