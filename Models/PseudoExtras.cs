fe07120aafa19a7d91101f4a8ead03e0666dc438
sasha-poirier-pix4d <sasha.poirier@pix4d.com>
2024-03-06 13:13:59 +0100

    PseudoExtras fixes + generator

--- Models/PseudoExtras.cs
+++ Models/PseudoExtras.cs
@@ -1,7 +1,6 @@
 /**
 Basically copy from Swift project
 */
-
 namespace PseudoExtras {
     public enum DiffStatus
     {
@@ -20,33 +19,39 @@ namespace PseudoExtras {
 
     record Satellite(
         int Id,  int Elv, int Azm, int Snr, bool Use, SatelliteType Type
-    );
+    ) {
+        public static Array Values = Enum.GetValues(typeof(SatelliteType));
+        private static readonly Random RNG = new ();
+        public static Satellite Random() {
+            return new Satellite(Id: RNG.Next(),Elv: RNG.Next(0, 180), Azm: RNG.Next(0, 360), Snr: RNG.Next(0, 120), true, (SatelliteType)Values.GetValue(RNG.Next(Values.Length)));
+        }
+    }
 
     record PseudoExtrasMessage(
         double latitude, double longitude, double altitude,
         float speed, float bearing,
-        float accuracy, float hdop, float vdop, float pdop,
+        float accuracy, float verticalAccuracyMeters, float hdop, float vdop, float pdop,
         float diffAge, DiffStatus diffStatus, int diffID,
         float vrms, float hrms,
         string receiverModel, string mockProvider,
-        double mslHeight, double undulation,
+        string geoidModel, double mslHeight, double undulation,
         float utcTime, string gpsTimeStamp, string utcTimeStamp,
         PseudoExtras.SubscriptionType subscriptionType,
         int satellites, int totalSatInView, 
         //Incorrect structure from documentation
         //int satellitesView,
         //int[] satellitesId, int[] satellitesElv, int[] satellitesAzm, int[] satellitesSnr, bool[] satellitesUse, SatelliteType[] satellitesType/*, Satellite[] satellitesMerged*/)
-        PseudoExtras.Satellite[] satteliteView)
+        PseudoExtras.Satellite[] satelliteView)
     {
         static PseudoExtrasMessage MakeZero() {
             return new PseudoExtrasMessage(latitude: 0, longitude: 0, altitude: 0, speed: 0, 
-                bearing: 0, accuracy: 0, hdop: 0, vdop: 0, pdop: 0,
+                bearing: 0, accuracy: 0, verticalAccuracyMeters: 0, hdop: 0, vdop: 0, pdop: 0,
                 diffAge: 0, diffStatus: DiffStatus.Unknown, diffID: 0,
-                vrms: 0, hrms: 0, receiverModel: "", mockProvider: "", mslHeight: 0, undulation: 0,
+                vrms: 0, hrms: 0, receiverModel: "", mockProvider: "", geoidModel: "", mslHeight: 0, undulation: 0,
                 utcTime: 0, gpsTimeStamp: "", utcTimeStamp: "",
                 subscriptionType: SubscriptionType.Free,
                 satellites: 0, totalSatInView: 0, 
-                satteliteView: []);
+                satelliteView: []);
                 //satellitesView: 0, satellitesId: new int[0], satellitesElv: new int[0], satellitesAzm: new int[0], satellitesSnr: new int[0], satellitesUse: new bool[0], satellitesType: new SatelliteType[0]);
         }
         
@@ -56,15 +61,16 @@ namespace PseudoExtras {
             var lat = 46.52955048375884 - (random.NextDouble() * (0.001 - -0.001) + -0.001);
             var lon = 6.600880505618732 - (random.NextDouble() * (0.001 - -0.001) + -0.001);
             var millis = DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
-            
-            return new PseudoExtrasMessage(latitude: lat, longitude: lon, altitude: 0, speed: 0, 
-                bearing: 0, accuracy: 0, hdop: 0, vdop: 0, pdop: 0,
+
+            //After 10 messages or so, send corrected...
+            return new PseudoExtrasMessage(latitude: lat, longitude: lon, altitude: 400, speed: 0, 
+                bearing: 0, accuracy: 0, verticalAccuracyMeters: 0, hdop: 0, vdop: 0, pdop: 0,
                 diffAge: 0, diffStatus: DiffStatus.Unknown, diffID: 0,
-                vrms: 0, hrms: 0, receiverModel: "", mockProvider: "", mslHeight: 0, undulation: 0,
-                utcTime: millis, gpsTimeStamp: "", utcTimeStamp: "",
+                vrms: 1, hrms: 1, receiverModel: "Catalyst", mockProvider: "Trimble Mobile Manager", geoidModel: "Some Model", mslHeight: 400, undulation: 20,
+                utcTime: millis, gpsTimeStamp: "2024-02-13T14:49:49.0000000", utcTimeStamp: "2024-02-13T14:49:49.0000000",
                 subscriptionType: SubscriptionType.Free,
                 satellites: 0, totalSatInView: 0,
-                satteliteView: [new Satellite(1, 20, 20, 20, true, SatelliteType.GPS)]);
+                satelliteView: [new Satellite(1, 20, 20, 20, false, SatelliteType.GPS)]);
                 //satellitesView: 0, satellitesId: new int[0], satellitesElv: new int[0], satellitesAzm: new int[0], satellitesSnr: new int[0], satellitesUse: new bool[0], satellitesType: new SatelliteType[0]);
         }
     }
